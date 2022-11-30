2fe58f8f80b472a00051e59ecdf839d0f442ce27
LynXz <115795138+LynXzl@users.noreply.github.com>
2022-11-30 23:04:05 +0800

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 2622527242 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/LynXzl/Scripts/main/SCP%20rBreach/Script.lua"))()
+  elseif game.PlaceId == 70005410 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/LynXzl/Scripts/main/Blox%20Hunt/Script.lua"))()
+  elseif game.PlaceId == 5865858426 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/LynXzl/Scripts/main/Retail%20Tycoon%202/AutoBuy.lua"))()
+end
