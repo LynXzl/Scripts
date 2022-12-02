65564ee750c661bc2c0a16b38f0c999bbd599fc0
LynXz <115795138+LynXzl@users.noreply.github.com>
2022-12-02 21:49:17 +0800

    Update PhilipsCoinsAutoFarm.lua

--- Retail Tycoon 2/PhilipsCoinsAutoFarm.lua
+++ Retail Tycoon 2/PhilipsCoinsAutoFarm.lua
@@ -1,13 +1,2 @@
 local Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/LynXzl/Tools/main/Functions.lua"))()
-
-if game:GetService("Workspace").Map:FindFirstChild("Holiday") then
-    Functions:Notify("LynXz","Autofarm Started","10")
-    for i,v in pairs(game:GetService("Workspace").Map.Holiday.Items:GetChildren()) do
-        repeat 
-          game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = v.CFrame
-          game.Players.LocalPlayer.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
-          wait(0.1)
-        until v.Transparency == 1
-    end
-    Functions:Notify("LynXz","Autofarm Ended","5")
-end
+Functions:Notify("LynXz","Event ended!","15")
