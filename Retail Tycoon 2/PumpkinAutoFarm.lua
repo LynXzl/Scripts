a9330702cf6d9034cfecf6c9b59a0d81226d2491
LynXz <115795138+LynXzl@users.noreply.github.com>
2022-12-02 21:48:47 +0800

    Update PumpkinAutoFarm.lua

--- Retail Tycoon 2/PumpkinAutoFarm.lua
+++ Retail Tycoon 2/PumpkinAutoFarm.lua
@@ -1,18 +1,2 @@
 local Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/LynXzl/Tools/main/Functions.lua"))()
 Functions:Notify("LynXz","Halloween event ended!","15")
---[[
-if game:GetService("Workspace").Map:FindFirstChild("Holiday") then
-    Functions:Notify("LynXz","Autofarm Started","10")
-    for i,v in pairs(game:GetService("Workspace").Map.Holiday.Items:GetChildren()) do
-        if v.Pumpkin.Transparency == 0 then
-            game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = v.Pumpkin.CFrame
-            game.Players.LocalPlayer.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
-            wait(0.15)
-            Functions:Touch(v.Pumpkin)
-        end
-    end
-    Functions:Notify("LynXz","Autofarm Ended","5")
-else
-    Functions:Notify("LynXz","Event has ended","10")
-end
-]]--
