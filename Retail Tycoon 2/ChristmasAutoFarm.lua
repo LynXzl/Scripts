b28ea3a57f18502e43e284f7e46d70f0fbffe801
LynXz <115795138+LynXzl@users.noreply.github.com>
2022-12-16 18:21:40 +0800

    Update ChristmasAutoFarm.lua

--- Retail Tycoon 2/ChristmasAutoFarm.lua
+++ Retail Tycoon 2/ChristmasAutoFarm.lua
@@ -1,4 +1,7 @@
 local Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/LynXzl/Tools/main/Functions.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/LynXzl/UI-Libraries/main/SolarisUI.lua"))()
+
+Library:Notification("🎅LynX", "AutoFarm Started!")
 
 for i,v in ipairs(game:GetService("Workspace").Map.Holiday.Items:GetChildren()) do
 game.Players.LocalPlayer.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
@@ -8,3 +11,5 @@ wait()
 Functions:Touch(v) 
 wait()
 end
+
+Library:Notification("🎅LynX", "AutoFarm Ended!")
